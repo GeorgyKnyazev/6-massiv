513862b0105582857faf3aa868a04d4644da4f25
Георгий Князев <saint_georgy@yahoo.com>
2022-07-05 16:24:55 +0300

    1

--- 6 massiv/Program.cs
+++ 6 massiv/Program.cs
@@ -8,52 +8,45 @@ namespace _6_massiv
         {
             int maxRandomNamber = 100;
             int minRandomNamber = 10;
-            int minNumberInArray = int.MaxValue;
-            int numberOrderInArray = 0;
             int[] userInputArray = new int[30];
-            int[] firstTempArray = new int[userInputArray.Length];
-            
-
             Random random = new Random();
 
             for (int i = 0; i < userInputArray.Length; i++)
             {
-                userInputArray[i] = random.Next(minRandomNamber,maxRandomNamber);
-                if(userInputArray[i] < minNumberInArray)
-                {
-                    minNumberInArray = userInputArray[i];
-                }
+                userInputArray[i] = random.Next(minRandomNamber, maxRandomNamber);
                 Console.Write(userInputArray[i] + " ");
             }
 
-            while(numberOrderInArray < userInputArray.Length)
+            
+            for (int i = 0; i < userInputArray.Length; i++)
             {
-                
-                int numberRepitInArray = 0;
+                int minNumberInArray = int.MaxValue;
+                int numberOrderInArray = 0;
+                int tempNumber;
 
-                for (int i = 0; i < userInputArray.Length; i++)
+                for (int j = i; j < userInputArray.Length; j++)
                 {
-                    if (userInputArray[i] == minNumberInArray)
+                    if(userInputArray[j] < minNumberInArray)
                     {
-                        numberRepitInArray++;
+                        minNumberInArray = userInputArray[j];
+                        numberOrderInArray = j;
+                    }
+                    else
+                    {
+                        continue;
                     }
                 }
-                if (numberRepitInArray != 0)
-                {
-                    for (int i = 0; i < numberRepitInArray; i++)
-                    {
-                        firstTempArray[numberOrderInArray] = minNumberInArray;
-                        numberOrderInArray++;
-                    }
-                }
-                minNumberInArray++;
+                tempNumber = userInputArray[i];
+                userInputArray[i] = minNumberInArray;
+                userInputArray[numberOrderInArray] = tempNumber;
             }
+            
 
             Console.WriteLine();
 
-            for (int i = 0; i < firstTempArray.Length; i++)
+            for (int i = 0; i < userInputArray.Length; i++)
             {
-                Console.Write(firstTempArray[i] + " ");
+                Console.Write(userInputArray[i] + " ");
             }
         }
     }
